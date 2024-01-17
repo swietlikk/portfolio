fc3f6cadda7e9d9651b8d0ce7e534ad6d7995c22
swietlikk <adamos.swietlicki@gmail.com>
2024-01-17 01:57:08 +0100

    awesome header section

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Section1 from './components/section1/Section1';
+
+function App() {
+    return (
+        <div>
+            <Section1 />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/section1/section1.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './Section1.scss';
+
+function Section1() {
+    return (
+        <section className="open__section">
+            <div className="background-color">
+            </div>
+            <header className="header">
+                <div className="header__inner">
+                    <div className="logo__names">
+                    <a href="" className="logo logo__name">Adam</a>
+                    <a href="" className="logo logo__surname">Świetlicki</a>
+                    </div>
+                    <input type="checkbox" className="hamburger__select" />
+                    <div className="hamburger">
+                        <span className="hamburger__icon"></span>
+                        <span className="hamburger__icon"></span>
+                        <span className="hamburger__icon"></span>
+                    </div>
+                    <nav className="navigation">
+                        <a href="" className="navigation__item navigation__item--active">
+                            <span className="navigation__highlight">ABOUT ME</span>
+                        </a>
+                        <a href="" className="navigation__item">
+                            <span className="navigation__highlight">PROJECTS</span>
+                        </a>
+                        <a href="" className="navigation__item">
+                            <span className="navigation__highlight">CONTACT</span>
+                        </a>
+                    </nav>
+                </div>
+            </header>
+
+            <div className="intro">
+                <h2 className="intro__hdl">Hello</h2>
+                <h1 className="intro__name">I'm Adam</h1>
+                <p className="intro__dsc">FRONT END DEVELOPER</p>
+                <a href="" className="intro__btn btn">HIRE ME</a>
+            </div>
+
+            <div className="image">
+                <img src="../../../src/images/me.png" alt="" className="image__img"/>
+            </div>
+        </section>
+    );
+}
+
+export default Section1;
